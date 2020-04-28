d35340c218f505ab66ad21f439525567b69e0441
PhilippErler <e01426424@student.tuwien.ac.at>
2020-04-28 23:15:22 +0200

    put RX cars first so that rally cars overwrite duplicates -> priority for rally

--- source/dr2/car_data.py
+++ source/dr2/car_data.py
@@ -1,6 +1,38 @@
 # see also: https://github.com/soong-construction/dirt-rally-time-recorder/blob/master/setup-dr2.sql
 # max_rpm, idle_rpm, max_gears, car_name
 car_data = [
+    # Crosskarts
+    [1598.547119140625, 161.26841735839844, 6.0, 'Speedcar Xtrem'],
+
+    # RX Group B
+    # [890.1179809570312, 167.55160522460938, 5.0, 'Lancia Delta S4 RX'],  # same as Peugeot 208 R2, non-rx
+    [942.477783203125, 167.55160522460938, 5.0, 'Ford RS200 Evolution'],
+    # [837.758056640625, 209.43951416015625, 5.0, 'Peugeot 205 T16 Evo 2 RX'],  # same as non-rx
+    [994.8377075195312, 115.19173431396484, 5.0, 'MG Metro 6R4 RX'],
+
+    # RX2
+    [837.758056640625, 167.55160522460938, 6.0, 'Ford Fiesta OMSE SuperCar Lites'],
+
+    # RX Supercars
+    [874.4099731445312, 209.43951416015625, 6.0, 'Volkswagen Polo R Supercar'],
+    [874.4099731445312, 209.43951416015625, 6.0, 'Audi S1 EKS RX quattro'],
+    [837.758056640625, 178.02359008789062, 6.0, 'Peugeot 208 WRX'],
+    [816.81414794921875, 172.78759765625, 5.0, 'Renault Megane RS'],
+    [811.578125, 188.49555969238281, 6.0, 'Ford Fiesta RX (MK8)'],
+    [785.398193359375, 172.78759765625, 6.0, 'Ford Fiesta RX (MK7)'],
+    [774.92620849609375, 178.02359008789062, 6.0, 'Subaru WRX STI RX'],  # same as Skoda Fabia Rally
+
+    # RX Supercars 2019
+    [837.758056640625, 178.02359008789062, 5.0, 'Renault Megane R.S. RX'],  # same as Subaru Impreza WRX STI NR4
+    [837.758056640625, 178.02359008789062, 6.0, 'Peugeot 208 WRX'],  # same as Peugeot 208 WRX
+    [874.4099731445312, 209.43951416015625, 6.0, 'Audi S1 EKS RX Quattro'],  # same as Audi S1 EKS RX quattro
+    [837.758056640625, 178.02359008789062, 6.0, 'Renault Clio R.S. RX'],  # same as Peugeot 208 WRX
+    [837.758056640625, 188.49555969238281, 6.0, 'Ford Fiesta RXS Evo 5'],  # same as Ford Fiesta RX (Stard)
+    [811.578125, 188.49555969238281, 6.0, 'Ford Fiesta RX (MK8)'],  # same as Ford Fiesta RX (MK8)
+    [785.398193359375, 261.79940795898438, 6.0, 'Mini Cooper SX1'],
+    [837.758056640625, 188.49555969238281, 6.0, 'Ford Fiesta RX (Stard)'],  # same as Ford Fiesta RXS Evo 5
+    [785.398193359375, 178.02359008789062, 6.0, 'Seat Ibiza RX'],  # same as Ford Focus RS Rally 2001
+
     # H1 FWD
     [733.03826904296875, 83.77580261230469, 4.0, 'Mini Cooper S'],
     [628.31854248046875, 104.71975708007812, 4.0, 'Citroen DS 21'],
@@ -62,8 +94,8 @@ car_data = [
     [774.92620849609375, 188.49555969238281, 6.0, 'Citroen C4 Rally'],
     [774.92620849609375, 178.02359008789062, 6.0, 'Skoda Fabia Rally'],  # same as Subaru WRX STI RX
     [769.69024658203125, 186.92477416992188, 5.0, 'Ford Focus RS Rally 2007'],
-    [785.398193359375, 219.91148376464844,  6.0, 'Subaru Impreza 2008'],
-    [785.398193359375, 178.02359008789062,  6.0, 'Ford Focus RS Rally 2001'],
+    [785.398193359375, 219.91148376464844, 6.0, 'Subaru Impreza 2008'],
+    [785.398193359375, 178.02359008789062, 6.0, 'Ford Focus RS Rally 2001'],  # same as Seat Ibiza RX
     [837.758056640625, 204.2035369873047,  6.0, 'Subaru Impreza 2001'],
     [680.678466796875, 157.0796356201172,  5.0, 'Peugeot 206 Rally'],
     [816.8141479492188, 207.34512329101562, 6.0, 'Subaru Impreza S4 Rally'],
@@ -87,38 +119,6 @@ car_data = [
     [994.8377075195312, 188.49555969238281, 6.0, 'Volkswagen Polo S1600'],
     [968.6577758789062, 198.96754455566406, 6.0, 'Renault Clio RS S1600'],
     [994.8377075195312, 198.96754455566406, 6.0, 'Opel Corsa Super 1600'],
-
-    # Crosskarts
-    [1598.547119140625, 161.26841735839844, 6.0, 'Speedcar Xtrem'],
-
-    # RX Group B
-    # [890.1179809570312, 167.55160522460938, 5.0, 'Lancia Delta S4 RX'],  # same as Peugeot 208 R2, non-rx
-    [942.477783203125, 167.55160522460938, 5.0, 'Ford RS200 Evolution'],
-    # [837.758056640625, 209.43951416015625, 5.0, 'Peugeot 205 T16 Evo 2 RX'],  # same as non-rx
-    [994.8377075195312, 115.19173431396484, 5.0, 'MG Metro 6R4 RX'],
-
-    # RX2
-    [837.758056640625, 167.55160522460938, 6.0, 'Ford Fiesta OMSE SuperCar Lites'],
-
-    # RX Supercars
-    [874.4099731445312, 209.43951416015625, 6.0, 'Volkswagen Polo R Supercar'],
-    [874.4099731445312, 209.43951416015625, 6.0, 'Audi S1 EKS RX quattro'],
-    [837.758056640625, 178.02359008789062, 6.0, 'Peugeot 208 WRX'],
-    [816.81414794921875, 172.78759765625, 5.0, 'Renault Megane RS'],
-    [811.578125, 188.49555969238281, 6.0, 'Ford Fiesta RX (MK8)'],
-    [785.398193359375, 172.78759765625, 6.0, 'Ford Fiesta RX (MK7)'],
-    [774.92620849609375, 178.02359008789062, 6.0, 'Subaru WRX STI RX'],  # same as Skoda Fabia Rally
-
-    # RX Supercars 2019
-    [837.758056640625, 178.02359008789062, 5.0, 'Renault Megane R.S. RX'],  # same as Subaru Impreza WRX STI NR4
-    [837.758056640625, 178.02359008789062, 6.0, 'Peugeot 208 WRX'],  # same as Peugeot 208 WRX
-    [874.4099731445312, 209.43951416015625, 6.0, 'Audi S1 EKS RX Quattro'],  # same as Audi S1 EKS RX quattro
-    [837.758056640625, 178.02359008789062, 6.0, 'Renault Clio R.S. RX'],  # same as Peugeot 208 WRX
-    [837.758056640625, 188.49555969238281, 6.0, 'Ford Fiesta RXS Evo 5'],  # same as Ford Fiesta RX (Stard)
-    [811.578125, 188.49555969238281, 6.0, 'Ford Fiesta RX (MK8)'],  # same as Ford Fiesta RX (MK8)
-    [785.398193359375, 261.79940795898438, 6.0, 'Mini Cooper SX1'],
-    [837.758056640625, 188.49555969238281, 6.0, 'Ford Fiesta RX (Stard)'],  # same as Ford Fiesta RXS Evo 5
-    [785.398193359375, 178.02359008789062, 6.0, 'Seat Ibiza RX'],
 ]
 
 car_dict = dict()
